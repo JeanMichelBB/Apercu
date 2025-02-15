99fef4f88cf945281af87556480a58d1a41cf34c
JeanMichelBB <jeanmichelbberube@outlook.com>
2025-02-15 18:24:34 -0400

    update ENV secret in github action

--- first-look/src/api.jsx
+++ first-look/src/api.jsx
@@ -1,3 +1,3 @@
 // src/api.jsx
-export const apiKey = import.meta.env.VITE_API_KEY;
-export const apiUrl = import.meta.env.VITE_API_URL;
+export const apiKey = import.meta.env.VITE_APP_API_KEY;
+export const apiUrl = import.meta.env.VITE_APP_API_URL;
