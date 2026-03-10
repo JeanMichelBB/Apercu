a2cd5e647dea066dd22416f1004ab9b8fe5e75b4
JeanMichelBB <jeanmichelbberube@outlook.com>
2026-03-09 23:54:30 -0400

    refactor: simplify header structure and improve styling for responsiveness

--- first-look/src/components/Header/Header.jsx
+++ first-look/src/components/Header/Header.jsx
@@ -17,21 +17,15 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <div className="left-container">
-          {/* <a href="/" className="link"><h2 className='text-logo'>{t('header.logo')}</h2></a> */}
-          <Link to="/" className="Link"><h2 className='text-logo'>{t('header.logo')}</h2></Link>
-        </div>
-        <div className="right-container">
-          <Link to="/service" className="link">{t('header.services')}</Link>
-          <Link to="/pricing" className="link">{t('header.pricing')}</Link>
-          <a href="https://jeanmichelbb.github.io/" className="link">{t('header.portfolio')}</a>
-          <Link to="/about" className="link">{t('header.about')}</Link>
-          <Link to="/contact" className="link">{t('header.contact')}</Link>
-          {/* Language button */}
-          <button className="language-button" onClick={() => handleLanguageChange(otherLanguage)}>
-            {otherLanguage.toUpperCase()}
-          </button>
-        </div>
+        <Link to="/" className="Link"><h2 className='text-logo'>{t('header.logo')}</h2></Link>
+        <Link to="/service" className="link">{t('header.services')}</Link>
+        <Link to="/pricing" className="link">{t('header.pricing')}</Link>
+        <a href="https://jeanmichelbb.github.io/" className="link">{t('header.portfolio')}</a>
+        <Link to="/about" className="link">{t('header.about')}</Link>
+        <Link to="/contact" className="link">{t('header.contact')}</Link>
+        <button className="language-button" onClick={() => handleLanguageChange(otherLanguage)}>
+          {otherLanguage.toUpperCase()}
+        </button>
       </div>
     </header>
   );
